ee4247d0e4cd8ce8bb10545bdef7d20c14029640
Arrematai <magodosleiloes@gmail.com>
2024-11-10 18:51:36 -0300

    cors flask

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from APIcopart import Copart
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app, origins=["https://www.arremataisolucoes.com.br"])
 
 @app.route('/search', methods=['POST'])
 def search():
