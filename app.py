d09a7665bbf8298818ebb74a7fd3c52ca260673d
Arrematai <magodosleiloes@gmail.com>
2024-11-11 15:20:40 -0300

    add Palacio

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # from flask import Flask, request, jsonify
 # from APIcopart import Copart
+# from APIsodresantoro import SodreSantoro
 # from flask_cors import CORS
 # import os
 
@@ -9,16 +10,27 @@
 # @app.route('/search', methods=['POST'])
 # def search():
 #     data = request.json
-#     query = data.get('query')  # Recebe a query enviada pelo cliente
-#     return Copart(query)       # Retorna a resposta da função como JSON
+#     query = data.get('query')
+
+#     # Realiza as duas consultas
+#     re_copart = Copart(query) or [] # Chama Copart e extrai o JSON
+#     re_sodre_santoro = SodreSantoro(query) or []  # Chama SodreSantoro e extrai o JSON
+
+#     # Mescla os resultados
+#     re_mesclados = re_copart + re_sodre_santoro
+
+#     # Retorna o JSON combinado
+#     return jsonify(re_mesclados)
+
+
 
 # if __name__ == '__main__':
-#     port = int(os.getenv("PORT", 5000))
 #     app.run(host='0.0.0.0', port=5000)
 
 from flask import Flask, request, jsonify
 from APIcopart import Copart
 from APIsodresantoro import SodreSantoro
+from APIpalacio import Palacio_dos_leiloes
 from flask_cors import CORS
 import os
 
@@ -32,10 +44,11 @@ def search():
 
     # Realiza as duas consultas
     re_copart = Copart(query) or [] # Chama Copart e extrai o JSON
-    re_sodre_santoro = SodreSantoro(query) or []  # Chama SodreSantoro e extrai o JSON
+    re_sodre_santoro = SodreSantoro(query) or []
+    re_palacio_dos_leiloes = Palacio_dos_leiloes(query) or []# Chama SodreSantoro e extrai o JSON
 
     # Mescla os resultados
-    re_mesclados = re_copart + re_sodre_santoro
+    re_mesclados = re_copart + re_sodre_santoro + re_palacio_dos_leiloes
 
     # Retorna o JSON combinado
     return jsonify(re_mesclados)
