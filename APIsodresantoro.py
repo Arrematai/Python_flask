62db696d63707c1dbf86545c025d6572e8017756
Arrematai <magodosleiloes@gmail.com>
2024-11-10 23:47:15 -0300

    heares sodre

--- APIsodresantoro.py
+++ APIsodresantoro.py
@@ -81,7 +81,14 @@ def SodreSantoro(query):
         "content-type": "application/json",
         "origin": "https://www.sodresantoro.com.br",
         "priority": "u=1, i",
-        "referer": "https://www.sodresantoro.com.br/"
+        "referer": "https://www.sodresantoro.com.br/",
+        "sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36"
     }
 
     response = requests.post(url, data=payload, headers=headers)
