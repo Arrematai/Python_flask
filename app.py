ec0f0f1c34a270574f656005e07db97e03c8e188
Arrematai <magodosleiloes@gmail.com>
2024-11-13 20:27:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-from flask import Flask, request, jsonify
-from APIjoaoemilio import Joao_Emilio
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://www.arremataisolucoes.com.br"}})
-
-@app.route('/test_joao_emilio', methods=['POST', 'OPTIONS'])
-def test_joao_emilio():
-    # Se for uma requisição OPTIONS, responde com status 200 e os headers CORS apropriados
-    if request.method == 'OPTIONS':
-        response = app.make_response('', 200)
-        response.headers.add("Access-Control-Allow-Origin", "https://www.arremataisolucoes.com.br")
-        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-        return response
-
-    # Processa a requisição POST
-    data = request.json
-    query = data.get('query')
-    re_joao_emilio = Joao_Emilio(query) or []
-    response = jsonify(re_joao_emilio)
-    response.headers.add("Access-Control-Allow-Origin", "https://www.arremataisolucoes.com.br")
-    return response
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
-
-
-
-
 # from flask import Flask, request, jsonify
 # from APIcopart import Copart
 # from APIpalacio import Palacio_dos_leiloes
@@ -61,6 +28,39 @@ if __name__ == '__main__':
 
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+from flask import Flask, request, jsonify
+from APIcopart import Copart
+from APIpalacio import Palacio_dos_leiloes
+from APIsodresantoro import SodreSantoro
+from APIjoaoemilio import Joao_Emilio
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["https://www.arremataisolucoes.com.br"])
+
+@app.route('/search', methods=['POST'])
+def search():
+    data = request.json
+    query = data.get('query')
+
+    # # Realiza as duas consultas
+    # re_copart = Copart(query) or [] # Chama Copart e extrai o JSON
+    # re_sodre_santoro = SodreSantoro(query) or []
+    # re_palacio_dos_leiloes = Palacio_dos_leiloes(query) or []# Chama SodreSantoro e extrai o JSON
+    re_joao_emilio = Joao_Emilio(query) or []
+
+    # # Mescla os resultados
+    # re_mesclados = re_copart + re_sodre_santoro + re_palacio_dos_leiloes + re_joao_emilio
+    # print(re_joao_emilio)
+
+    # Retorna o JSON combinado
+    return jsonify(re_joao_emilio)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
