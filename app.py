876405eb7b7c99968dadf523036429174d3fe663
Arrematai <magodosleiloes@gmail.com>
2024-11-16 10:46:12 -0300

    site

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ from APIjoaoemilio import Joao_Emilio
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=*, supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"], allow_headers=["Content-Type", "Authorization"])
+CORS(app, origins=[""https://arrematai-yrdl5ev0e0uq9lap.builder-preview.com/""], 
+supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"], allow_headers=["Content-Type", "Authorization"])
 
 @app.route('/search', methods=['POST'])
 def search():
