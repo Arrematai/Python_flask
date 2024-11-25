2ef6584224092d2640f327d9cb8141421c5d6f75
Arrematai <magodosleiloes@gmail.com>
2024-11-25 16:03:21 -0300

    commit

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def search():
     re_superbid = SuperBid(query) or []
     re_rogerio_menezes = Rogeriomenezes(query) or []
     # # Mescla os resultados
-    re_mesclados = re_copart + re_sodre_santoro + re_palacio_dos_leiloes + re_joao_emilio + re_superbid + re_rogerio_menezes
+    re_mesclados = re_copart + re_sodre_santoro + re_palacio_dos_leiloes + re_joao_emilio
 
     random.shuffle(re_mesclados)
     SaveML(re_mesclados, "BDML.json")
